fc09138a98e3e09933e11b7a91da466b9b10a276
Brian Alberg <xirov@xirov.com>
2017-05-01 15:29:59 +0200

    Main function now reads a config file and sets defaults using the config-rs crate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::env;
+use std::path;
+extern crate config;
+
 fn main() {
-    println!("Hello, world!");
+    let mut conf_file = path::PathBuf::new();
+    let mut conf = config::Config::new();
+
+    println!("Starting Cuckoo...");
+
+    // Find and read config file
+    match env::home_dir() { 
+        Some(path) => { conf_file = path; },
+        None => println!("Unable to find home directory. Config file not found")
+    }
+
+    conf_file.push(".config");
+    conf_file.push("cuckoo");
+    conf_file.push("config.toml");
+
+    println!("Reading config file: {}", conf_file.display());
+    
+    // Set config default values
+    conf.set_default("name", "John Doe");
+    conf.set_default("maildir", "");
+    
+    // Get values from config file
+    conf.merge(config::File::new(conf_file.to_str().unwrap(), config::FileFormat::Toml)).unwrap();
+
+    // println!("{:?}", conf.get("name"));
 }
