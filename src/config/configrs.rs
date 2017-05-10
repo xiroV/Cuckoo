8a479dfe403c370da9c69958e2ac108a833ca320
Brian Alberg <xirov@xirov.com>
2017-05-10 14:56:33 +0200

    Modularized the ConfigReader, making it easy to implement new config parsers (hopefully).

--- src/config/configrs.rs
+++ src/config/configrs.rs
@@ -1,25 +1,7 @@
+use config::{Account, Config, ConfigReader};
 use std::env;
 use std::path;
 extern crate config as conf;
-
-#[derive(Default)]
-pub struct Account {
-    pub id: String,
-    pub name: Option<String>,
-    pub mail: Option<String>,
-    pub imap_server: Option<String>,
-    pub imap_user: Option<String>,
-}
-
-pub struct Config {
-    pub accounts: Vec<Account>,    
-}
-
-pub trait ConfigReader {
-    fn new() -> Self;
-    fn read(&mut self) -> Option<&str>;
-}
-
 
 impl ConfigReader for Config {
     fn new() -> Self {
