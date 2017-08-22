3029d8263db46c20b4d8faf1d97ae9836c57f79e
Brian Alberg <xirov@xirov.com>
2017-08-22 12:42:51 +0200

    Added some message structures for the control layer

--- src/control/mod.rs
+++ src/control/mod.rs
@@ -2,11 +2,25 @@ use imap::{IMAP, Connection, IMAPError}
 
 mod simple_controller;
 
+enum MessageType {
+    message,
+    error, 
+}
+
 struct Control {
     imap_connection: Some(IMAP<Connection>),
 }
 
+struct ControlMessage {
+    msg_type: 
+    description: String;
+}
+
 trait Controller {
     fn new() -> Self;
-    fn imap_connect() -> Result<Self, ()>; // FIXME Should not return () on error
+    fn imap_connect(&self, &String) -> Result<Self, ControlMessage>;
+}
+
+trait MessageHandler {
+    fn new(&str) -> Self;
 }
