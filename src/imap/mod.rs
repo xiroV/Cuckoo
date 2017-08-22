d4b0446b2055bd429dd022f611bc9e9545c345ff
Brian Alberg <xirov@xirov.com>
2017-08-22 10:17:20 +0200

    Removed some unused commented lines

--- src/imap/mod.rs
+++ src/imap/mod.rs
@@ -3,20 +3,9 @@
  * New IMAP clients should implement the IMAPClient trait
  */
 
-//extern crate imap;
-//extern crate openssl;
-
-// Currently used IMAP client
+// Currently used IMAP client and connection
 pub mod rust_imap;
 pub use imap::rust_imap::Connection;
-
-/*
-use self::openssl::ssl::{SslConnectorBuilder, SslMethod, SslStream};
-use self::imap::client::Client;
-use std::net::TcpStream;
-*/
-
-use config::{Account};
 
 pub trait IMAPClient {
     fn connect(&String, &String, &String) -> Result<IMAP<Connection>, IMAPError>;
