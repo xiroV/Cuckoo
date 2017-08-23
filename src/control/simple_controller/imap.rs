0b5b651b9690e291864b4783f100ce0450c532cb
Brian Alberg <xirov@xirov.com>
2017-08-23 12:31:45 +0200

    Updated the IMAP controller to support the new config structure and ConfigError handling

--- src/control/simple_controller/imap.rs
+++ src/control/simple_controller/imap.rs
@@ -1,57 +1,68 @@
 use imap::{IMAP, IMAPClient, IMAPErrorType, IMAPErrorHandler};
-use control::{Control, IMAPController, ControlMessage, MessageHandler, MessageType};
-use config::{Config, ConfigReader};
+use control::{Control, Controller, IMAPController, ControlMessage, MessageHandler, MessageType};
+use config::{Config, ConfigReader, ConfigError, ConfigErrorType};
 
 impl IMAPController for Control {
     fn imap_connect(&mut self, account: &String, password: &String) -> Result<&Self, ControlMessage> {
-        let mut conf = Config::new();
+        let mut conf = Config::init();
 
         match conf.read() {
-            None => { },
-            Some(err) => println!("{}", err)
+            Ok(_) => { },
+            Err(e) => {
+                match e.err_type {
+                    ConfigErrorType::MissingField => {
+                        return Err(
+                            ControlMessage::new(
+                                MessageType::error,
+                                &format!("Missing account field '{:?}' in config file", e.field)
+                            )
+                        )
+                    },
+                    ConfigErrorType::FileNotFound => {
+                        return Err(
+                            ControlMessage::new(
+                                MessageType::error,
+                                "Config file not found"
+                            )
+                        )
+                    }
+                };
+            }
         }
 
         // FIXME This is a mess
         match conf.get_account(account) {
             Some(acc) => {
-                match acc.imap_server {
-                    Some(imap_server) => {
-                        match acc.imap_user {
-                            Some(imap_user) => {
-                                println!("Connecting..");
+                self.push_message(ControlMessage::new(MessageType::message, "Connecting.."));
 
-                                match IMAP::connect(&imap_server, &imap_user, &password) {
-                                    Ok(conn) => {
-                                        self.imap_connection = Some(conn);
-                                        return Ok(self);
-                                    },
-                                    Err(e) => match e.err_type {
-                                        IMAPErrorType::Connection => Err(
-                                            ControlMessage::new(
-                                                MessageType::error,
-                                                "Connection Failed")
-                                            ),
-                                        IMAPErrorType::Login => Err(
-                                            ControlMessage::new(
-                                                MessageType::error,
-                                                "Login failed"
-                                            )
-                                        ),
-                                    }
-                                }
-                            },
-                            None => {
-                                return Err(ControlMessage::new(MessageType::error, "IMAP user not found in config"));
-                            }
-                        }
+                match IMAP::connect(&acc.imap_server, &acc.imap_user, &password) {
+                    Ok(conn) => {
+                        self.imap_connection = Some(conn);
+                        return Ok(self);
                     },
-                    None => {
-                        return Err(ControlMessage::new(MessageType::error, "imap server not found in config"));
-                    }
+                    Err(e) => match e.err_type {
+                        IMAPErrorType::Connection => Err(
+                            ControlMessage::new(
+                                MessageType::error,
+                                "Connection Failed"
+                            )
+                        ),
+                        IMAPErrorType::Login => Err(
+                            ControlMessage::new(
+                                MessageType::error,
+                                "Login failed"
+                            )
+                        ),
+                    },
                 }
             },
             None => {
-                return Err(ControlMessage::new(MessageType::error, "account not found in config"));
+                return Err(
+                    ControlMessage::new(
+                        MessageType::error,
+                        "account not found in config"
+                    )
+                );
             }
         }
     }
